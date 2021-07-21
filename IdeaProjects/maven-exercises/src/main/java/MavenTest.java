4d89dd29baf953e7c143e3af1a8f429c9858726a
cruby9339@gmail.com <christian.ruby72@gmail.com>
2021-07-21 09:28:28 -0500

    adds maven exercises

---
+++ IdeaProjects/maven-exercises/src/main/java/MavenTest.java
@@ -0,0 +1,21 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class MavenTest {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        String userInput = sc.nextLine();
+        System.out.println(StringUtils.isNumeric(userInput));
+
+        System.out.println("Please enter a string");
+        String userInput2 = sc.nextLine();
+        System.out.println(StringUtils.swapCase(userInput2));
+        System.out.println(StringUtils.reverse(userInput2));
+
+    }
+
+}
